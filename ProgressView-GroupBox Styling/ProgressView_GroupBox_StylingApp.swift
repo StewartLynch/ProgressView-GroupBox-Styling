54ff7efc2cb3d3015ab03492fb15fc2d34f89c44
Stewart Lynch <slynch@createchsol.com>
2020-10-28 08:51:18 -0700

    Initial Commit

---
+++ ProgressView-GroupBox Styling/ProgressView_GroupBox_StylingApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProgressView_GroupBox_StylingApp.swift
+//  ProgressView-GroupBox Styling
+//
+//  Created by Stewart Lynch on 2020-10-28.
+//
+
+import SwiftUI
+
+@main
+struct ProgressView_GroupBox_StylingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
