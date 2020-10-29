effc9502f50310b191a4c17ffbfd972b886a3197
Stewart Lynch <slynch@createchsol.com>
2020-10-28 17:04:51 -0700

    Completed Project

--- ProgressView-GroupBox Styling/ContentView.swift
+++ ProgressView-GroupBox Styling/ContentView.swift
@@ -8,9 +8,27 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var progress: CGFloat = 0
     var body: some View {
-        Text("Hello, world!")
+        GroupBox(label: Text("Charging Station")) {
+            VStack {
+                ProgressView(value: progress) {
+                    Label("Battery", systemImage: "battery.25")
+                } currentValueLabel: {
+                    Text("Currrent Charge")
+                }
+                .progressViewStyle(BatteryViewStyle(width: 100, height: 25))
+                Button("Charge") {
+                    progress = 0
+                    withAnimation(Animation.linear(duration: 2)) {
+                        progress = 1
+                    }
+                }
+                .buttonStyle(SpecialButtonStyle(actionType: .confirm, withImage: false))
+            }
             .padding()
+        }
+        .groupBoxStyle(ColoredGroupBoxStyle(backgroundColor: UIColor.systemGreen, width: 300, labelColor: .purple, opacity: 0.2))
     }
 }
 
